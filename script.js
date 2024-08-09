7ede07655b1f1f94cc8c9332e19716c3553136fd
melmsilva <106822477+melmsilva@users.noreply.github.com>
2024-08-09 10:41:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const caixaPrincipal = document.querySelectorAll("caixa-pricipal");
+const caixaPerguntas = document.querySelectorAll("caixa-perguntas");
+const caixaAlternativas = document.querySelectorAll("caixa-alternativas");
+const caixaResultado = document.querySelectorAll("caixa-resultado");
+const textoResultado = document.querySelectorAll("texto-resultdo");
+
+const perguntas = ["Pergunta 1"]
